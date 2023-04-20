c314e94587e4f80678aeb0525de99cc124b8e9e8
«Zhenya» <saltykovazh@mail.ru>
2023-04-20 23:58:50 +0400

    HW1.2

--- Homework/Homework1.java
+++ Homework/Homework1.java
@@ -1,71 +1,57 @@
+import java.util.Arrays;
 import java.util.Random;
 
-public class Homework01.java {
+// 1. Выбросить случайное целое число в диапазоне от 0 до 2000 и сохранить в i
+// 2. Посчитать и сохранить в n номер старшего значащего бита выпавшего числа
+// 3. Найти все кратные n числа в диапазоне от i до Short.MAX_VALUE сохранить в массив m1
+// 4. Найти все некратные n числа в диапазоне от Short.MIN_VALUE до i и сохранить в массив m2
+
+public class Homework1 {
+
     public static void main(String[] args) {
-
-        // 1. Выбросить случайное целое число в диапазоне от 0 до 2000 и сохранить в i
-        int i = get_zero_to_2000_random();
-
-        // 2. Посчитать и сохранить в n номер старшего значащего бита выпавшего числа
-        int n = get_num_highest_bit(i);
-
-        // 3. Найти все кратные n числа в диапазоне от i до Short.MAX_VALUE сохранить в массив m1
-        int[] m1 = get_divisible_array(i, n);
-
-        // 4. Найти все некратные n числа в диапазоне от Short.MIN_VALUE до i и сохранить в массив m2
-        int[] m2 = get_not_divisible_array(i, n);
-
-        System.out.println("i = " + i + ", n = " + n);
-        System.out.println("Длина массива m1 = " + m1.length + ", m2 = " + m2.length);
-       //  print_array(m1);
-       // print_array(m2);
+        
+        int i = get_random();
+        int n = get_bit(i);
+// Задача 3 
+        short[] m1 = get_arraym1(n, i);
+        System.out.println("Массив m1" + Arrays.toString(m1));
+// Задача 4
+        short[] m2 = get_arraym2(n, i);
+        System.out.println("Массив m2" +Arrays.toString(m2));
+// Задача 1
+        System.out.println("i = " + i);   //Изменила последовательность вывода Задач, чтобы был виден ответ на 1 и 2
+// Задача 2
+        System.out.println("n = " + n);
+    }
+    
+    static int get_random(){   
+        return new Random().nextInt(0,2001);
     }
 
-    public static int get_zero_to_2000_random() {
-        return new Random().nextInt(0, 2001);
+    static int get_bit(int i){
+        return Integer.toBinaryString(i).length();    
     }
 
-    public static int get_num_highest_bit(int in_number) {
-        return Integer.toBinaryString(in_number).length();
-    }
-
-    public static int[] get_divisible_array(int in_number, int in_divider) {
-        if (in_divider == 0) return new int[0];
-        StringBuilder sb = new StringBuilder();
-        for (int i = in_number; i <= Short.MAX_VALUE; i++)
-            if (i % in_divider == 0) sb.append(i).append(" ");
-        if (sb.length() > 0) {
-            String[] strings = sb.toString().split(" ");
-            int[] result = new int[strings.length];
-
-            for (int i = 0; i < strings.length; i++) {
-                result[i] = Integer.parseInt(strings[i]);
-            }
-            return result;
-        } else {
-            return new int[0];
+    static short[] get_arraym1(int n, int i){
+        int n0 = ((i - 1) / n + 1) * n;
+        int n1 = (Short.MAX_VALUE / n) * n;
+        short[] arr = new short[(n1 - n0) / n + 1];
+        for (int l = n0; l <= n1; l += n) {
+            arr[(l - n0) / n] = (short) l;
         }
+        return arr;
     }
 
-    public static int[] get_not_divisible_array(int in_number, int in_divider) {
-        if (in_divider == 0 || in_divider == 1) return new int[0];
-        StringBuilder sb = new StringBuilder();
-        for (int i = Short.MIN_VALUE; i <= in_number; i++)
-            if (i % in_divider != 0) sb.append(i).append(" ");
-        if (sb.length() > 0) {
-            String[] strings = sb.toString().split(" ");
-            int[] result = new int[strings.length];
-
-            for (int i = 0; i < strings.length; i++) {
-                result[i] = Integer.parseInt(strings[i]);
+    static short[] get_arraym2(int n, int i){
+        int k = (i  / n ) + (Short.MIN_VALUE / n ) * (-1) + 1;
+        short[] arr = new short[i - Short.MIN_VALUE + 1 - k];
+        int m = 0;
+        for (int l = Short.MIN_VALUE; l <= i; l++) {
+            if (l % n != 0) {
+                arr[m] = (short) l;
+                m++;
             }
-            return result;
-        } else {
-            return new int[0];
         }
-    }
-
-    public static void print_array(int[] in_array) {
-        for (int i : in_array) System.out.print(i + " ");
+        return arr;
     }
 }
